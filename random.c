663e2a392583ba8bf41a179d66e6c4840d354766
Emmanuel Kourtakis <emmanuel.kourtakis@temple.edu>
2024-08-30 16:44:28 -0400

    created body of randchar()

--- random.c
+++ random.c
@@ -2,5 +2,6 @@
 #include "header.h"
 
 char randchar() {
-
+    return (char) (rand() % 26 + 65); // generates random number between 0 and 25
+                               // 'A' corresponds to 65 in ASCII codes, so add 65
 }
